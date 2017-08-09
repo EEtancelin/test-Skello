431406543dfc29cb84409bc838cede7db43677e9
16Q45T <etancelin.edouard@gmail.com>
2017-08-09 15:06:41 +0200

    Toogle color of thumb when i click on it .

--- public/vueModule/home.js
+++ public/vueModule/home.js
@@ -1,6 +1,6 @@
 var data = { counter: 0 };
 Vue.component('thumbs', {
-  template: '<button v-bind:class="{liked: isliked}" v-on:click="toogleLike">{{ isLiked }}</button>',
+  template: '<div class="fs-l glyphicon glyphicon-thumbs-up" aria-hidden="true"v-bind:class="{liked: this.isLiked}" v-on:click="toogleLike"></div>',
   // data is technically a function, so Vue won't
   // complain, but we return the same object
   // reference for each component instance
@@ -8,11 +8,11 @@ Vue.component('thumbs', {
     return { isLiked: true }
   },
   methods: {
-        toogleLike: function(){
-            this.isLiked = !this.isLiked;
-          // some code to filter users
-        }
-      }
+    toogleLike: function(){
+        this.isLiked = !this.isLiked;
+      // some code to filter users
+    }
+  }
 })
 
 
